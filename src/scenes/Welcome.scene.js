0cf6ff382a6bcc6b2a2c79bfedf2c208054aa024
Alex Vargas Benamburg <yo@vbalex.com>
2021-02-11 14:11:31 -0600

    testing window

--- src/scenes/Welcome.scene.js
+++ src/scenes/Welcome.scene.js
@@ -1,8 +1,8 @@
 import * as THREE from 'three';
 
-import TestScene from "./Test.scene";
+import TestScene from './Test.scene';
 
-import RegistrationScene from "./registration.scene";
+import RegistrationScene from './registration.scene';
 
 // Loaders
 var textureLoader = new THREE.TextureLoader();
@@ -34,14 +34,15 @@ function WelcomeScene(setScene) {
   initLights();
   initObjects();
 
-  var added = false; // objects not added before the mouse 
-  function overTheWindow(envet) { // hover 
+  var added = false; // objects not added before the mouse
+  function overTheWindow(envet) {
+    // hover
     if (!added) {
       scene.add(bin);
       added = true;
       setTimeout(() => {
-        scene.add(DareToEnter)
-      }, 1000)
+        scene.add(DareToEnter);
+      }, 1000);
     }
   }
 
@@ -59,47 +60,65 @@ function WelcomeScene(setScene) {
   }
 
   function go() {
-      var ns = RegistrationScene(setScene);
-      setScene(ns);
+    var ns = RegistrationScene(setScene);
+    setScene(ns);
   }
 
   function initObjects() {
     var plane = new THREE.PlaneGeometry(1080 / 400, 1920 / 400);
-    var texture = new THREE.TextureLoader().load('static/imgs/welcome_page/background.png');
-    var materialBackground = new THREE.MeshBasicMaterial({ map: texture, transparent: true });
+    var texture = new THREE.TextureLoader().load(
+      'static/imgs/welcome_page/background.png'
+    );
+    var materialBackground = new THREE.MeshBasicMaterial({
+      map: texture,
+      transparent: true,
+    });
     background = new THREE.Mesh(plane, materialBackground);
     background.position.set(0, 0, 0);
     scene.add(background);
 
-    var planeWindow = new THREE.PlaneGeometry(229 / 1250, 431 / 1250);
-    var textureWindow = new THREE.TextureLoader().load('static/imgs/welcome_page/window.png');
-    var materialwindowOfHouse = new THREE.MeshBasicMaterial({ map: textureWindow, transparent: true });
-    windowOfHouse = new THREE.Mesh(planeWindow, materialwindowOfHouse);
-    windowOfHouse.position.set(0.1, 0.48, 0.01);
-    scene.add(windowOfHouse);
-    windowOfHouse.on("mouseover", overTheWindow);
-    windowOfHouse.on("touchstart", overTheWindow);
-
-
     var planeBin = new THREE.PlaneGeometry(447 / 1400, 822 / 1400);
-    var textureBin = new THREE.TextureLoader().load('static/imgs/welcome_page/bin.png');
-    var materialBin = new THREE.MeshBasicMaterial({ map: textureBin, transparent: true });
+    var textureBin = new THREE.TextureLoader().load(
+      'static/imgs/welcome_page/bin.png'
+    );
+    var materialBin = new THREE.MeshBasicMaterial({
+      map: textureBin,
+      transparent: true,
+    });
     bin = new THREE.Mesh(planeBin, materialBin);
     bin.position.set(0.8, -0.5, 0);
 
+    var planeWindow = new THREE.PlaneGeometry(229 / 1250, 431 / 1250);
+    var textureWindow = new THREE.TextureLoader().load(
+      'static/imgs/welcome_page/window.png'
+    );
+    var materialwindowOfHouse = new THREE.MeshBasicMaterial({
+      map: textureWindow,
+      transparent: true,
+    });
+    windowOfHouse = new THREE.Mesh(planeWindow, materialwindowOfHouse);
+    windowOfHouse.position.set(0.1, 0.48, 0.1);
+    scene.add(windowOfHouse);
+    windowOfHouse.on('mouseover', overTheWindow);
+    windowOfHouse.on('touchstart', overTheWindow);
+
     var planeDareToEnter = new THREE.PlaneGeometry(2163 / 1200, 1459 / 1200);
-    var textureDareToEnter = new THREE.TextureLoader().load('static/imgs/welcome_page/DareToEnter.png');
-    var materialDareToEnter = new THREE.MeshBasicMaterial({ map: textureDareToEnter, transparent: true });
+    var textureDareToEnter = new THREE.TextureLoader().load(
+      'static/imgs/welcome_page/DareToEnter.png'
+    );
+    var materialDareToEnter = new THREE.MeshBasicMaterial({
+      map: textureDareToEnter,
+      transparent: true,
+    });
     DareToEnter = new THREE.Mesh(planeDareToEnter, materialDareToEnter);
     DareToEnter.position.set(-0.3, -1.8, 0.01);
 
     DareToEnter.cursor = 'pointer';
     DareToEnter.on('click', go);
     DareToEnter.on('touchstart', go);
-
   }
 
-  function update() { }
+  function update() {}
 
   return {
     scene,
