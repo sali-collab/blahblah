26f626527450a221b2ace2deefb979122465c952
Sarah Ali <sarahali@Students-MacBook-Air.local>
2021-02-14 15:54:41 -0600

    placement of asset change

--- src/scenes/glimpses.scene.js
+++ src/scenes/glimpses.scene.js
@@ -19,6 +19,9 @@ var Shoe;
 var Text;
 var Line;
 
+var SafetyText;
+var HealthText;
+var EducationText;
 
 function glimpsesScene(setScene,color) {
   camera = new THREE.PerspectiveCamera(
@@ -77,12 +80,33 @@ function goSafety() {
 
 
 
-   var planeText = new THREE.PlaneGeometry(581/300, 169/300);
+   var planeText = new THREE.PlaneGeometry(893/300, 241/300);
    var textureText = new THREE.TextureLoader().load('static/imgs/glimpses_page/Text.png');
    var materialText  = new THREE.MeshBasicMaterial({ map: textureText  , transparent:true});
    Text  = new THREE.Mesh(planeText , materialText );
-   Text .position.set(0,-2, 0);
+   Text .position.set(0,-2.5, 0);
    scene.add(Text); 
+
+   var planeSafetyText = new THREE.PlaneGeometry(87/300, 26/300);
+   var textureSafetyText = new THREE.TextureLoader().load('static/imgs/glimpses_page/SafetyText.png');
+   var materialSafetyText  = new THREE.MeshBasicMaterial({ map: textureSafetyText  , transparent:true});
+   SafetyText  = new THREE.Mesh(planeSafetyText , materialSafetyText );
+   SafetyText .position.set(-0.2,2,0);
+   scene.add(SafetyText); 
+
+   var planeEducationText = new THREE.PlaneGeometry(131/300, 22/300);
+   var textureEducationText = new THREE.TextureLoader().load('static/imgs/glimpses_page/EducationText.png');
+   var materialEducationText  = new THREE.MeshBasicMaterial({ map: textureEducationText  , transparent:true});
+   EducationText  = new THREE.Mesh(planeEducationText , materialEducationText );
+   EducationText.position.set(-1.5,-1.8, 0);
+   scene.add(EducationText); 
+
+   var planeHealthText = new THREE.PlaneGeometry(87/300, 23/300);
+   var textureHealthText = new THREE.TextureLoader().load('static/imgs/glimpses_page/HealthText.png');
+   var materialHealthText  = new THREE.MeshBasicMaterial({ map: textureHealthText  , transparent:true});
+   HealthText  = new THREE.Mesh(planeHealthText , materialHealthText);
+   HealthText.position.set(0,-1.25, 0);
+   scene.add(HealthText); 
 
   var planeLine = new THREE.PlaneGeometry(612/300, 53/300);
   var textureLine= new THREE.TextureLoader().load('static/imgs/glimpses_page/Line.png');
@@ -117,7 +141,7 @@ function goSafety() {
   var textureBaby = new THREE.TextureLoader().load('static/imgs/glimpses_page/Baby.png');
   var materialBaby = new THREE.MeshBasicMaterial({ map: textureBaby , transparent:true});
   Baby = new THREE.Mesh(planeBaby, materialBaby);
-  Baby.position.set(-1.5,-1.5, 0.01);
+  Baby.position.set(-1.5,-1.45, 0.01);
   scene.add(Baby); 
 
   Baby.cursor = 'pointer';
