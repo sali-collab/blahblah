9dcb506e05f0f57c66536790c63a05d9074a5778
Sarah Ali <sarahali@Students-MacBook-Air.local>
2021-02-17 20:40:28 -0600

    added blinking highlight

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { WEBGL } from './webgl'; // if browser is compatible to 3js
 import { Interaction } from 'three.interaction'; // for clicks and stuff on 3js
 import { initDatabase } from './firebase';
 
-import titleScene from './scenes/title.scene';
+import HealthcareScene from './scenes/Healthcare.scene';
 
 if (WEBGL.isWebGLAvailable()) {
   var renderer;
@@ -30,7 +30,7 @@ if (WEBGL.isWebGLAvailable()) {
     document.body.appendChild(renderer.domElement);
     window.addEventListener('resize', onWindowResize, false);
     document.addEventListener('touchmove', preventBehavior, { passive: false });
-    var firstScene = titleScene(setScene);
+    var firstScene = HealthcareScene(setScene);
     setScene(firstScene);
   }
 
